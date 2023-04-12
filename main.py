5ef523eb449fdd9c6e557df4ccb07e3478a71645
weyltensor007 <weyltensor@gmail.com>
2023-04-12 13:22:49 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import os
+import configparser
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--token",
+    "-t",
+    type = str
+)
+
+args = parser.parse_args()
+
+target_url = "https://web3.dgpa.gov.tw/WANT03FRONT/AP/WANTF00003.aspx?GETJOB=Y"
+
+on_github = False # True -> 在 github 運作; False -> 在 local 運作; 要啟用 action 時要記得換回 True
+
+if on_github:
+    line_token = args.token
+else:
+    config = configparser.ConfigParser()
+    config.read('line_token.ini')
+    line_token =  config["token"]["value"]
+
+need_columns = ["ORG_NAME",  # 徵才機關
+                "RANK",  # 官職等
+                "TITLE",  # 職稱
+                "SYSNAM",  # 職系
+                "WORK_PLACE_TYPE",  # 工作地點
+                "VIEW_URL",  # 資訊網址
+                ]
+
+
+if on_github:
+    df = pd.read_xml(target_url, parser='etree') # default parser = lxml, 但不相容 poetry 環境
+    df = df.loc[:, need_columns]
+
+else:
+    data_csv = 'data.csv'
+    if os.path.exists(data_csv):
+        df = pd.read_csv(data_csv)
+        df = df.loc[:, need_columns]
+    else:
+        df = pd.read_xml(target_url, parser='etree')
+        df = df.loc[:, need_columns]
+        df.to_csv('data.csv', index=False)
+
+# make pandas query string
+pd_query_conditions = [
+    "SYSNAM == '經建行政'",
+    "RANK.str.contains('薦任')",
+    # "WORK_PLACE_TYPE.str.contains('臺中市|南投縣|臺北市')",
+]
+query_str = " and ".join(pd_query_conditions)
+
+
+df_filter = df.query(query_str)
+
+# 使用 view_url 裡面的 workid 進行排序, 排序完後 drop
+df_filter["work_id"] = df_filter["VIEW_URL"].apply(
+    lambda x: int(x.split("=")[-1]))
+df_filter = df_filter.sort_values(by="work_id", ascending=False)
+df_filter = df_filter.drop("work_id", axis=1)
+
+# insert hyperlink
+df_filter["VIEW_URL"] = df_filter["VIEW_URL"].apply(
+    lambda x: f"<a href={x}>事求人詳細資訊</a>"
+)
+print(df_filter.shape)
+# output html
+df_filter.to_html("result.html", index=False, render_links=True, escape=False)
